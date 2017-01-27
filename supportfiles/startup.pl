a9da450ea3881f4fe013c7ef474cd94b503aab60
Chris <chris@starforge.co.uk>
2017-01-27 09:50:14 +0000

    Add mod_perl startup file

---
+++ supportfiles/startup.pl
@@ -0,0 +1,43 @@
+use strict;
+
+use lib "/path/to/bigscreen/blocks";
+use lib "/path/to/bigscreen/modules";
+use lib "/var/www/webperl";
+
+# Make sure we are in a sane environment.
+$ENV{MOD_PERL} or die "not running under mod_perl!";
+
+# Preload core stuff
+use Apache2::RequestRec;
+use ModPerl::Util;
+
+# Preload frequently used modules to speed up client spawning.
+use CGI ();
+CGI->compile(':cgi');
+use CGI::Carp ();
+
+use Apache::DBI;
+use DBD::mysql ();
+use MIME::Base64 ();
+use List::BinarySearch ();
+use List::Util ();
+
+# And load the core modules
+use BigScreen;
+use BigScreen::API;
+use BigScreen::AppUser;
+use BigScreen::BlockSelector;
+use BigScreen::Login;
+use BigScreen::SlideShow;
+use BigScreen::SlideSource::MondayMail;
+use BigScreen::SlideSource::Newsagent;
+use BigScreen::SlideSource::Twitter;
+use BigScreen::SlideSource;
+use BigScreen::SlideSource;
+use BigScreen::System::Metadata;
+use BigScreen::System::Roles;
+use BigScreen::System::SlideSource;
+use BigScreen::System::Tags;
+use BigScreen::System;
+use BigScreen::Userbar;
+1;
