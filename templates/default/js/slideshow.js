5d5356d0c2095efe92614e6737178c0026434b1b
Chris <chris@starforge.co.uk>
2017-02-02 15:22:48 +0000

    Allow for multiple looping before page reload

--- templates/default/js/slideshow.js
+++ templates/default/js/slideshow.js
@@ -5,17 +5,18 @@ $(function() {
                                           Seconds: { show: true }
                                         },
                                   count_past_zero: false,
-                                  total_duration: duration
+                                  total_duration: (delays.maxDelay / 1000)
                                 });
 
-    new Foundation.TimedOrbit($('#slideshow'), delays);
     $('#slideshow').on('slidechange.zf.timedorbit', function(event, newslide, delay) {
         $('#timer').data('timer', delay / 1000);
         $('#timer').TimeCircles().restart();
 
-        // if moving from last to first, reload the page
-        if( newslide.data('slide') == 0) {
+        // if moving from last to first, and we've done enough loops, reload the page
+        if((newslide.data('slide') == 0) && (--loops < 0)) {
             location.reload();
         }
     });
+
+    new Foundation.TimedOrbit($('#slideshow'), delays);
 });
